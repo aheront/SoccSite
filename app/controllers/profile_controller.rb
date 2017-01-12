1c9f85068bee36af2993eb76d771f2523bf9d63b
aheront <aheronth@gmail.com>
2017-01-12 16:14:49 +0200

    shtuka persha

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,22 +5,18 @@ class ProfileController < ApplicationController
   end
 
   def new
-
   end
-  def show
 
+  def show
     @user = User.find(params[:id])
   end
 
   def create
-
   end
 
   def update
-
   end
 
   def destroy
-
   end
 end
