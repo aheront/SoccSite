d34debe013eb390ddd5b64fc5a4a3c7068728d69
aheront <aheronth@gmail.com>
2017-01-13 10:57:14 +0200

    run nested

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email,
-                                 :password, :password_confirmation,:birthday,:sex,:photo)
+                                 :password, :password_confirmation,
+                                 :birthday,:sex,photo_attributes:[:image])
   end
 
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email,
-                                 :password, :password_confirmation, :current_password,:birthday,:sex,:photo)
+                                 :password, :password_confirmation,
+                                 :current_password,:birthday,:sex,photo_attributes:[:image,:id])
   end
 end
