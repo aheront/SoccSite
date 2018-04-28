80c01330317112a2a5eed241f65bd3cd7736e6f8
aheront <aheronth@gmial.ocm>
2018-04-28 20:20:26 +0300

    mina deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,10 @@ set :domain, '46.101.227.102'
 set :deploy_to, '/var/www/soccsite'
 set :repository, 'git@github.com:aheront/SoccSite.git'
 set :branch, 'master'
-set :rvm_use_path, '/usr/local/rvm/scripts/rvm'
+#set :rvm_use_path, '/usr/local/rvm/scripts/rvm'
 
 # Optional settings:
-   set :user, 'root'          # Username in the server to SSH to.
+   set :user, 'soccsite'          # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
   set :forward_agent, true     # SSH forward_agent.
 
